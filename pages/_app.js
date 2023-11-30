5396d95917cabb38e94559fd7178e565348f0bf3
Yumie Tsuzuki <yumie.t.29@gmail.com>
2023-11-30 11:25:38 -0500

    20231130-configure basic settings

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Open_Sans } from "@next/font/google";
+
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  variable: "--font-openSans",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${openSans.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "bg-color-1": "#fff",
+        "bg-color-2": "#f3f4f6",
+        "text-color-1": "#0a0a0a",
+        "text-color-2": "#a3a3a3",
+        "accent-color-1": "#fda4af",
+        "accent-color-2": "#f43f5e",
+      },
+      fontFamily: {
+        sans: ["var(--font-openSans)"],
       },
     },
   },
   plugins: [],
-}
+};

cab87caee8e7a553323fd438ab1679a0f2f1473a
Yumie Tsuzuki <yumie.t.29@gmail.com>
2023-12-06 17:18:00 -0500

    change favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
+import Head from "next/head";
 import Layout from "../components/layout";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <link rel="shortcut icon" href="/logo.svg" />
+      </Head>
+
       <Component {...pageProps} />
     </Layout>
   );

315ff32b114a01c3caec0c8cc091126323d8d965
Yumie Tsuzuki <yumie.t.29@gmail.com>
2024-01-02 15:32:04 -0500

    20240102-update footer

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,7 @@ function Footer() {
     <footer className="h-32 px-4 sm:px-7 md:px-12 lg:px-24 py-7 sm:py-12 border-t flex flex-col sm:flex-row items-center sm:justify-between">
       <Navigation />
       <p className="text-sm text-text-color-3 mt-5 sm:mt-0">
-        © 2023, Yumie Tsuzuki
+        © 2024, Yumie Tsuzuki
       </p>
     </footer>
   );
